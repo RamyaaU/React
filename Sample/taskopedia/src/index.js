13eca76b0f5d19226f987059ea51f860bad0cf2b
RamyaU <ramyayadav2016@gmail.com>
2024-08-09 23:58:47 +0530

    cretaed first component

--- Sample/taskopedia/src/index.js
+++ Sample/taskopedia/src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//   </React.StrictMode>
+// );
+
+// root.render(<div>Learn React</div>)
+
+//component
+function MainBody()
+{
+  return (
+  <div>
+    <p>TaskOPedia</p>
+    <ul>
+      <li>ABC</li>
+      <li>DEF</li>
+    </ul>
+  </div>
+)
+}
+
+//rendering teh component
+root.render(<MainBody />);
